e9a31f1857c8d758a3580f5d3bafb11bf384751f
AndreasHansson1 <32000349+AndreasHansson1@users.noreply.github.com>
2018-04-11 15:18:34 +0200

    testing

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php echo 'hello world!';
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>andreas_hansson_journal</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+</body>
+</html>
